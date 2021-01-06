1cab3453a562f9b41cd71044624e604f80f96d55
Hemang Sharma <59247602+hemangsharma@users.noreply.github.com>
2021-01-06 11:59:21 +0530

    Add files via upload

---
+++ NewYearSwiftUI/NewYearSwiftUI/NewYearSwiftUIApp.swift
@@ -0,0 +1,19 @@
+//
+//  NewYearSwiftUIApp.swift
+//  NewYearSwiftUI
+//  linkedin: sharmahemang
+//  twitter: hemang0401
+//  instagram: tribunalwolf
+//
+
+
+import SwiftUI
+
+@main
+struct NewYearSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
